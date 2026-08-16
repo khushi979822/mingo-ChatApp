4ab7ad9aa82be25275d7b3b8b4f8d2d3b4205567
khushi979822 <khushikumari5114@gmail.com>
2026-08-16 16:42:00 +0530

    Add Home component and update styling for better layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SiteHeader from "./components/SiteHeader";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <SiteHeader />
+      <Home />
     </>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="bg-primary p-2">Welcome to My Website</div>
+      <div className="bg-base-200 p-2 h-[90vh] flex items-center justify-center">
+        Welcome to My Website
+      </div>
     </>
   );
 };
